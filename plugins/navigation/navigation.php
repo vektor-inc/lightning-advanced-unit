166b76e6e638b6a8152a074ec00d4a9dfee275c2
kurudrive <kurudrive@gmail.com>
2016-06-11 01:43:34 +0900

    スライドメニュー on/off のバグ修正

--- plugins/navigation/navigation.php
+++ plugins/navigation/navigation.php
@@ -24,41 +24,48 @@ function lightning_adv_unit_customize_register_menu_type($wp_customize) {
   ));
 }
 
+function ltg_adv_is_slide_menu(){
+  $options = get_option('lightning_theme_options');
+  if ( !isset( $options['menu_type'] ) || $options['menu_type'] != 'vertical_show_hide' ){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 /*-------------------------------------------*/
-/*  Change Menu Type
+/*  Load js & CSS
 /*-------------------------------------------*/
-$options = get_option('lightning_theme_options');
-if ( ! ( isset( $options['menu_type'] ) && $options['menu_type'] == 'vertical_show_hide' ) ){
-  /*-------------------------------------------*/
-  /*  Load js & CSS
-  /*-------------------------------------------*/
-  add_action( 'wp_head','ltg_adv_nav_add_js' );
-  function ltg_adv_nav_add_js() {
+add_action( 'wp_head','ltg_adv_nav_add_js' );
+function ltg_adv_nav_add_js() {
+  if ( ltg_adv_is_slide_menu() ) {
     wp_register_script( 'ltg_adv_nav_js' , LIGHTNING_ADVANCED_URL.'plugins/navigation/js/navigation.js', array( 'jquery' ), LIGHTNING_ADVANCED_VERSION );
     wp_enqueue_script( 'ltg_adv_nav_js' );
     wp_enqueue_style( 'ltg_adv_nav_style_css', LIGHTNING_ADVANCED_URL.'plugins/navigation/css/navigation.css', array(), LIGHTNING_ADVANCED_VERSION, 'all' );
   }
+}
 
-  /*-------------------------------------------*/
-  /*  insert_header_before_html
-  /*-------------------------------------------*/
-  add_action( 'lightning_header_before', 'ltg_adv_insert_header_before_html' );
-  function ltg_adv_insert_header_before_html(){ ?>
-    <div id="bodyInner">
-        <section id="navSection" class="navSection">
-        <?php get_search_form(); ?>
-        </section>
-    <div id="wrap">
-  <?php
+/*-------------------------------------------*/
+/*  insert_header_before_html
+/*-------------------------------------------*/
+add_action( 'lightning_header_before', 'ltg_adv_insert_header_before_html' );
+function ltg_adv_insert_header_before_html(){
+  if ( ltg_adv_is_slide_menu() ) {
+    echo '<div id="bodyInner">';
+        echo '<section id="navSection" class="navSection">';
+        echo get_search_form();
+        echo '</section>';
+    echo '<div id="wrap">';
   }
+}
 
-  /*-------------------------------------------*/
-  /*  insert_footer_after_html
-  /*-------------------------------------------*/
-  add_action( 'lightning_footer_after', 'ltg_adv_insert_footer_after_html' );
-  function ltg_adv_insert_footer_after_html(){ ?>
-    </div><!-- [ /#wrap ] -->
-    </div><!-- [ /#bodyInner ] -->
-  <?php
+/*-------------------------------------------*/
+/*  insert_footer_after_html
+/*-------------------------------------------*/
+add_action( 'lightning_footer_after', 'ltg_adv_insert_footer_after_html' );
+function ltg_adv_insert_footer_after_html(){ 
+  if ( ltg_adv_is_slide_menu() ) {
+    echo '</div><!-- [ /#wrap ] -->';
+    echo '</div><!-- [ /#bodyInner ] -->';
   }
 }
