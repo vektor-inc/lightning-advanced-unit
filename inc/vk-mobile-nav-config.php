63e0adaf1049bc8d623c136dcdd9a5fba6fe4e89
kurudrive <kurudrive@gmail.com>
2018-01-31 19:28:50 +0900

    [ mobile nav ] 翻訳・説明追加

--- inc/vk-mobile-nav-config.php
+++ inc/vk-mobile-nav-config.php
@@ -7,6 +7,6 @@ if ( ! class_exists( 'Vk_Mobile_Nav' ) ) {
 	require_once( 'vk-mobile-nav/class-vk-mobile-nav.php' );
 
 	global $vk_mobile_nav_textdomain;
-	$vk_mobile_nav_textdomain = 'lightning';
+	$vk_mobile_nav_textdomain = 'lightning-adv-unit';
 
 }
