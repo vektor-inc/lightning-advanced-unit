a0ca930fb77608db887a5c51e88ac56bfe135e8f
kurudrive <kurudrive@gmail.com>
2018-03-07 01:39:04 +0900

    [ Change version ] 3.0.3

--- lightning_advanced_unit.php
+++ lightning_advanced_unit.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Lightning Advanced Unit
  * Plugin URI: http://lightning.vektor-inc.co.jp/
- * Version: 3.0.2
+ * Version: 3.0.3
  * Author: Vektor,Inc.
  * Author URI: https://www.vektor-inc.co.jp
  * Description: This is a plug-ins that extend the functionality of the theme "Lightning".
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:
 Tags: Lightning,
 Requires at least: 4.5
 Tested up to: 4.9.1
-Stable tag: 3.0.2
+Stable tag: 3.0.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
