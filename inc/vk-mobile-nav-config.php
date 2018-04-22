2b7e916a37204ce383b4b96aeb7ed8eae1884021
catherine <catherine5th.avenue@gmail.com>
2018-04-23 08:29:40 +0900

    [ VK Mobile Fix Nav ]　途中だけど一回コミット

--- inc/vk-mobile-nav-config.php
+++ inc/vk-mobile-nav-config.php
@@ -10,3 +10,11 @@ if ( ! class_exists( 'Vk_Mobile_Nav' ) ) {
 	$vk_mobile_nav_textdomain = 'lightning-adv-unit';
 
 }
+
+// ページ下部に固定表示するメニュー
+if ( ! class_exists( 'Vk_Mobile_Fix_Nav' ) ) {
+	require_once( 'vk-mobile-nav/class-vk-mobile-fix-nav.php' );
+
+	global $vk_mobile_fix_nav_textdomain;
+	$vk_mobile_fix_nav_textdomain = 'lightning-adv-unit';
+}
