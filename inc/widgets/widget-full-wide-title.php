02df5d778032bbe4a928afd008438445f0ad2681
kurudrive <kurudrive@gmail.com>
2018-04-22 23:25:07 +0900

    [ full wide title ] 余白指定機能実装 #25

--- inc/widgets/widget-full-wide-title.php
+++ inc/widgets/widget-full-wide-title.php
@@ -28,10 +28,12 @@ class LTG_Full_Wide_Title extends WP_Widget {
 			'media_image_id'     => null,
 			'title_bg_color'     => '',
 			'title_font_color'   => '',
-			'title'              => null,
+			'title'              => '',
 			'after_widget'       => '',
 			'title_shadow_use'   => false,
 			'title_shadow_color' => '#000',
+			'margin_top'         => '0',
+			'margin_botton'      => '40px',
 		);
 		return wp_parse_args( (array) $args, $defaults );
 	}
@@ -40,31 +42,19 @@ class LTG_Full_Wide_Title extends WP_Widget {
 	/*  form
 	/*-------------------------------------------*/
 	public function form( $instance ) {
-		$instance = self::default_options( $instance );
-
-		// タイトルの入力
-		if ( isset( $instance['title'] ) && $instance['title'] ) {
-			$title = $instance['title'];
-		} else {
-			$title = '';
-		}
-
-		$id   = $this->get_field_id( 'title' );
-		$name = $this->get_field_name( 'title' );
+		$options = self::default_options( $instance );
+		// ※ wp_parse_args()かけてるのでisset不要
 
 		echo '<p>';
 		echo  __( 'Title:', LIGHTNING_ADVANCED_TEXTDOMAIN ) . '<br>';
-		printf(
-			'<input type="text" id="%s" name="%s" value="%s" />',
-			$id,
-			$name,
-			esc_attr( $title )
-		);
+		$id   = $this->get_field_id( 'title' );
+		$name = $this->get_field_name( 'title' );
+		echo '<input type="text" id="' . $id . '" name="' . $name . '" value="' . esc_attr( $options['title'] ) . '" />';
 		echo '</p>';
 
 		// サブタイトルの入力
-		if ( isset( $instance['text'] ) && $instance['text'] ) {
-			$text = $instance['text'];
+		if ( isset( $options['text'] ) && $options['text'] ) {
+			$text = $options['text'];
 		} else {
 			$text = '';
 		}
@@ -80,28 +70,28 @@ class LTG_Full_Wide_Title extends WP_Widget {
 		// title font color
 		echo '<p class="color_picker_wrap">' .
 			'<label for="' . $this->get_field_id( 'title_font_color' ) . '">' . __( 'Text color of the title:', LIGHTNING_ADVANCED_TEXTDOMAIN ) . '</label><br/>' .
-			'<input type="text" id="' . $this->get_field_id( 'title_font_color' ) . '" class="color_picker" name="' . $this->get_field_name( 'title_font_color' ) . '" value="' . esc_attr( $instance['title_font_color'] ) . '" /></p>';
+			'<input type="text" id="' . $this->get_field_id( 'title_font_color' ) . '" class="color_picker" name="' . $this->get_field_name( 'title_font_color' ) . '" value="' . esc_attr( $options['title_font_color'] ) . '" /></p>';
 
 		// Shadow Use
-		$checked = ( $instance['title_shadow_use'] ) ? ' checked' : '';
+		$checked = ( $options['title_shadow_use'] ) ? ' checked' : '';
 		echo '<p><input type="checkbox" id="' . $this->get_field_id( 'title_shadow_use' ) . '" name="' . $this->get_field_name( 'title_shadow_use' ) . '" value="true"' . $checked . ' >';
 		echo '<label for="' . $this->get_field_id( 'title_shadow_use' ) . '">' . __( 'Text shadow', LIGHTNING_ADVANCED_TEXTDOMAIN ) . '</label><br/></p>';
 
 		// Shadow color
 		echo '<p class="color_picker_wrap">' .
 			'<label for="' . $this->get_field_id( 'title_shadow_color' ) . '">' . __( 'Text shadow color:', LIGHTNING_ADVANCED_TEXTDOMAIN ) . '</label><br/>' .
-			'<input type="text" id="' . $this->get_field_id( 'title_shadow_color' ) . '" class="color_picker" name="' . $this->get_field_name( 'title_shadow_color' ) . '" value="' . esc_attr( $instance['title_shadow_color'] ) . '" /></p>';
+			'<input type="text" id="' . $this->get_field_id( 'title_shadow_color' ) . '" class="color_picker" name="' . $this->get_field_name( 'title_shadow_color' ) . '" value="' . esc_attr( $options['title_shadow_color'] ) . '" /></p>';
 
 		// bg color
 		echo '<p class="color_picker_wrap">' .
 			'<label for="' . $this->get_field_id( 'title_bg_color' ) . '">' . __( 'Title background color:', LIGHTNING_ADVANCED_TEXTDOMAIN ) . '</label><br/>' .
-			'<input type="text" id="' . $this->get_field_id( 'title_bg_color' ) . '" class="color_picker" name="' . $this->get_field_name( 'title_bg_color' ) . '" value="' . esc_attr( $instance['title_bg_color'] ) . '" /></p>';
+			'<input type="text" id="' . $this->get_field_id( 'title_bg_color' ) . '" class="color_picker" name="' . $this->get_field_name( 'title_bg_color' ) . '" value="' . esc_attr( $options['title_bg_color'] ) . '" /></p>';
 
 		$image = null;
 		// ちゃんと数字が入っているかどうか？
-		if ( is_numeric( $instance['media_image_id'] ) ) {
+		if ( is_numeric( $options['media_image_id'] ) ) {
 			// 数字だったら、その数字の画像を full サイズで取得
-				$image = wp_get_attachment_image_src( $instance['media_image_id'], 'full' );
+				$image = wp_get_attachment_image_src( $options['media_image_id'], 'full' );
 		}
 ?>
 
@@ -114,7 +104,7 @@ class LTG_Full_Wide_Title extends WP_Widget {
 <button class="button button-default button-block" style="display:block;width:100%;text-align: center; margin:4px 0;" onclick="javascript:vk_title_bg_image_addiditional(this);return false;"><?php _e( 'Set image', LIGHTNING_ADVANCED_TEXTDOMAIN ); ?></button>
 <button class="button button-default button-block" style="display:block;width:100%;text-align: center; margin:4px 0;" onclick="javascript:vk_title_bg_image_delete(this);return false;"><?php _e( 'Delete image', LIGHTNING_ADVANCED_TEXTDOMAIN ); ?></button>
 <div class="_form" style="line-height: 2em">
-	<input type="hidden" class="__id" name="<?php echo $this->get_field_name( 'media_image_id' ); ?>" value="<?php echo esc_attr( $instance['media_image_id'] ); ?>" />
+	<input type="hidden" class="__id" name="<?php echo $this->get_field_name( 'media_image_id' ); ?>" value="<?php echo esc_attr( $options['media_image_id'] ); ?>" />
 </div>
 </div>
 <script type="text/javascript">
@@ -151,6 +141,22 @@ var vk_title_bg_image_delete = function(e){
 }
 </script>
 <?php
+echo '<p>';
+echo  __( 'Margin Top', LIGHTNING_ADVANCED_TEXTDOMAIN ) . ' : ';
+$id   = $this->get_field_id( 'margin_top' );
+$name = $this->get_field_name( 'margin_top' );
+echo '<input type="text" id="' . $id . '" name="' . $name . '" value="' . esc_attr( $options['margin_top'] ) . '" /><br />';
+echo __( 'Ex', LIGHTNING_ADVANCED_TEXTDOMAIN ) . ') 0;';
+echo '</p>';
+
+echo '<p>';
+echo  __( 'Margin Bottom', LIGHTNING_ADVANCED_TEXTDOMAIN ) . ' : ';
+$id   = $this->get_field_id( 'margin_bottom' );
+$name = $this->get_field_name( 'margin_bottom' );
+echo '<input type="text" id="' . $id . '" name="' . $name . '" value="' . esc_attr( $options['margin_bottom'] ) . '" /><br />';
+echo __( 'Ex', LIGHTNING_ADVANCED_TEXTDOMAIN ) . ') 40px';
+echo '</p>';
+
 	}
 
 		/*-------------------------------------------*/
@@ -165,6 +171,8 @@ var vk_title_bg_image_delete = function(e){
 		$instance['text']               = wp_kses_post( $new_instance['text'] );
 		$instance['title_shadow_use']   = ( $new_instance['title_shadow_use'] ) ? true : false;
 		$instance['title_shadow_color'] = sanitize_hex_color( $new_instance['title_shadow_color'] );
+		$instance['margin_top']         = esc_attr( $new_instance['margin_top'] );
+		$instance['margin_bottom']      = esc_attr( $new_instance['margin_bottom'] );
 		return $new_instance;
 	}
 
@@ -172,7 +180,6 @@ var vk_title_bg_image_delete = function(e){
 	/*  functions
 	/*-------------------------------------------*/
 	public static function widget_outer_style( $instance ) {
-		$widget_outer_style = 'border:1px solid #f00;';
 
 		// 画像IDから画像のURLを取得
 		if ( ! empty( $instance['media_image_id'] ) ) {
@@ -195,6 +202,14 @@ var vk_title_bg_image_delete = function(e){
 		} elseif ( empty( $image ) && empty( $instance['title_bg_color'] ) ) {
 			$widget_outer_style = '';
 		}
+
+		if ( isset( $instance['margin_top'] ) && $instance['margin_top'] != '' ) {
+			$widget_outer_style .= 'margin-top:' . esc_attr( $instance['margin_top'] ) . ';';
+		}
+		if ( isset( $instance['margin_bottom'] ) && $instance['margin_bottom'] != '' ) {
+			$widget_outer_style .= 'margin-bottom:' . esc_attr( $instance['margin_bottom'] ) . ';';
+		}
+
 		return $widget_outer_style;
 	}
 
@@ -225,6 +240,28 @@ var vk_title_bg_image_delete = function(e){
 	/*-------------------------------------------*/
 	public function widget( $args, $instance ) {
 		$instance = self::default_options( $instance );
+
+		// テーマ側から .widget に対してmargin-bottomが付けられてしまっているので、
+		// ウィジェット個別で上下余白指定がある場合は .widgetの余白を打ち消す必要がある
+		$widget_outer_style = '';
+		if ( isset( $instance['margin_top'] ) && $instance['margin_top'] != '' ) {
+			$widget_outer_style .= 'margin-top:0;';
+		}
+		if ( isset( $instance['margin_bottom'] ) && $instance['margin_bottom'] != '' ) {
+			$widget_outer_style .= 'margin-bottom:0;';
+		}
+
+		if ( $widget_outer_style ) {
+			$dynamic_css = '#' . $args['widget_id'] . '.widget {' . $widget_outer_style . '}';
+			// $dynamic_css = trim( $dynamic_css );
+			// // convert tab and br to space
+			// $dynamic_css = preg_replace( '/[\n\r\t]/', '', $dynamic_css );
+			// // Change multiple spaces to single space
+			// $dynamic_css = preg_replace( '/\s(?=\s)/', '', $dynamic_css );
+			echo '<style type="text/css">' . $dynamic_css . '</style>';
+			// wp_add_inline_style( 'lightning-design-style', $dynamic_css );
+		}
+
 		echo $args ['before_widget'];
 		echo '<div class="widget_ltg_adv_full_wide_title_outer" style="' . esc_attr( $this->widget_outer_style( $instance ) ) . '">';
 		echo '<h2 class="widget_ltg_adv_full_wide_title_title" style="' . esc_attr( $this->widget_font_style( $instance ) ) . '">' . wp_kses_post( $instance['title'] ) . '</h2>';
