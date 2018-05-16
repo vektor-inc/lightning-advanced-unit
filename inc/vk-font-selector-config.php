372fe2fe4cee4114a72c7c3483a3d36ba8c472c4
catherine <catherine5th.avenue@gmail.com>
2018-05-16 11:04:32 +0900

    ディレクトリ名などを変更

---
+++ inc/vk-font-selector-config.php
@@ -0,0 +1,12 @@
+<?php
+
+/*-------------------------------------------*/
+/*  Load modules
+/*-------------------------------------------*/
+if ( ! class_exists( 'Vk_Font_Selector_Customize' ) ) {
+	require_once( 'vk-font-selector/class-vk-font-selector.php' );
+
+	global $vk_font_selector_textdomain;
+	$vk_font_selector_textdomain = 'lightning-adv-unit';
+
+}
